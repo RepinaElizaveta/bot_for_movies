0c091985f986af1d86511f100c20787b8d8a5fc4
Elizaveta <elizabeth.repina@yandex.ru>
2022-06-13 13:10:39 +0300

    final

--- main.py
+++ main.py
@@ -1,25 +1,68 @@
-import requests
+import requests # -- библиотека, необходимая для реализации api-запросов
 
 
+# Класс, в котором хранятся данные о фильме, полученные от пользователя
+# в ходе взаимодействия
 class RequestData:
     my_type = ""
     genre = 0
     ratingFrom = 0
     yearFrom = 0
     yearTo = 2022
+    keywords = ""
 
-    def __init__(self, my_type, genre, ratingFrom, yearFrom, yearTo):
+    def __init__(self, my_type, genre, ratingFrom, yearFrom, yearTo, keywords):
         self.my_type = my_type
         self.genre = genre
         self.ratingFrom = ratingFrom
         self.yearFrom = yearFrom
         self.yearTo = yearTo
+        self.keywords = keywords
 
 
+# Класс, в котором реализованы методы взаимодействия с пользователем
 class UserCommunication:
-    def dataRequestFromUser(self) -> RequestData:
-        print("Привет! Я - бот для поиска фильмов, телевизионных шоу, сериалов и мини-сериалов.\nЯ помогу найти то, что тебе точно понравится! Для того, чтобы я смог помочь,пожалуйста, ответь на несколько вопросов.")
-        what = int(input("Что ты хочешь посмотреть?\nВведи цифру:\n 1.Фильм\n 2.Телевизионное шоу\n 3.Сериал\n 4.Мини-сериал\n"))
+    def startCommunication(self) -> int:
+        ''' Открывающий общение с пользователем метод '''
+
+        print("Привет! Я - бот для поиска фильмов, телевизионных шоу, "
+              "сериалов и мини-сериалов.\n"
+              "Я помогу найти то, что тебе точно понравится! "
+              "Для того, чтобы я смог помочь,пожалуйста, "
+              "ответь на несколько вопросов.")
+        try:
+            trial = int(input("Пожалуйста, выбери цифру способа, "
+                              "которым ты хочешь найти фильм:\n"
+                               "1. По фильтрам\n"
+                               "2. По ключевым словам\n"))
+        except:
+            trial = int(input("Введно некорректное значение.\n"
+                              "Пожалуйста, выбери цифру способа, "
+                              "которым ты хочешь найти фильм:\n"
+                              "1. По фильтрам\n"
+                              "2. По ключевым словам\n"))
+        while trial not in range(0, 3):
+            try:
+                trial = int(input("Введно некорректное значение.\n"
+                              "Пожалуйста, выбери цифру способа, "
+                              "которым ты хочешь найти фильм:\n"
+                              "1. По фильтрам\n"
+                              "2. По ключевым словам\n"))
+            except:
+                trial = int(input("Введно некорректное значение.\n"
+                              "Пожалуйста, выбери цифру способа, "
+                              "которым ты хочешь найти фильм:\n"
+                              "1. По фильтрам\n"
+                              "2. По ключевым словам\n"))
+        return trial
+
+    # Метод, вызываемый в случае выбора пользователем поиска по фильтрам
+    def dataRequestFromUserByFilter(self) -> RequestData:
+        ''' Метод производит поиск по фильтрам '''
+
+        what = int(input("Что ты хочешь посмотреть?\n"
+                         "Введи цифру:\n 1.Фильм\n 2.Телевизионное шоу\n "
+                         "3.Сериал\n 4.Мини-сериал\n"))
         if what == 1:
             my_type = "FILM"
         if what == 2:
@@ -33,20 +76,30 @@ class UserCommunication:
         while int(what) >= 5:
             print("Нет такого значения.")
             what = int(input("Введи цифру заново:\n"))
-        genre = input("Какой жанр ты хочешь посмотреть?\nВведи цифру из списка:\n 1.Триллер\n 2.Драма\n 3.Криминал\n 4.Мелодрама\n 5.Детектив\n")
+        genre = input("Какой жанр ты хочешь посмотреть?\n"
+                      "Введи цифру из списка:\n 1.Триллер\n 2.Драма\n "
+                      "3.Криминал\n 4.Мелодрама\n 5.Детектив\n")
         while int(genre) >= 6:
             print("Нет такого значения.")
             genre = (input("Введи цифру заново:\n"))
-        ratingFrom = input("Рейтинг может быть от 0 до 10.\nЯ буду искать проивзведения с рейтингом не ниже:\n")
+        ratingFrom = input("Рейтинг может быть от 0 до 10.\n"
+                           "Я буду искать проивзведения с рейтингом не ниже:\n")
         while int(ratingFrom) >= 11:
             print("Нет такого значения.")
             ratingFrom = (input("Введи цифру заново:\n"))
         print("Выбери временной промежуток.")
         yearFrom = input("От какого года мне искать?\n")
         yearTo = input("На каком годе остановиться?\n")
-        return RequestData(my_type, genre, ratingFrom, yearFrom, yearTo)
+        return RequestData(my_type, genre, ratingFrom, yearFrom, yearTo, "")
+
+    # Метод, вызываемый в случае выбора пользователем поиска по ключевым словам
+    def dataRequestFromUserByKeywords(self) -> RequestData:
+        ''' Метод производит поиск по ключевым словам '''
+        keywords = input("Введи через запятую ключевые слова:\n")
+        return RequestData("", 0, 0, 0, 2022, keywords)
 
 
+# Класс, в котором реализованы методы взаимодействия с api
 class ApiRequester:
     def callApi(self,url_film_info,params):
         API_KEY = 'bda95582-a1ca-4627-ae54-fa46a777f350'
@@ -59,38 +112,112 @@ class ApiRequester:
         json_response = response.json()
         return json_response
 
-url_film_info = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/'
+class WantedFilm:
+    rating = 'Не найдено'
+    year = ''
+    countries = ''
+    genre = ''
+    description = ''
+
+    def __init__(self, rating, year, countries, genre, description):
+        self.rating = rating
+        self.year = year
+        self.countries = countries
+        self.genre = genre
+        self.description = description
+
+# -- Переменная, отвечающая за хранение url-ссылки запроса --
+url_film_info = ""
+# -- Переменная, отвечающая за хранение данных, полученных от пользователя --
+dataFromUser: RequestData
+
+keys_of_dict = []
+params = dict()
+
+ # -- Инициализация класса UserCommunication --
 userCommunicator: UserCommunication = UserCommunication()
-dataFromUser: RequestData = userCommunicator.dataRequestFromUser()
-params = {
-    'genres': dataFromUser.genre,
-    'type': dataFromUser.my_type,
-    'ratingFrom': dataFromUser.ratingFrom,
-    'yearFrom': dataFromUser.yearFrom,
-    'yearTo': dataFromUser.yearTo,
-}
+
+# !Начало общения с пользователем!
+choice = userCommunicator.startCommunication()
+
+# На основе выбранной траектории происходит зарос данных по определенной ссылке
+if choice == 1:
+    url_film_info = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/'
+    dataFromUser: RequestData = userCommunicator.dataRequestFromUserByFilter()
+    params = {
+        'genres': dataFromUser.genre,
+        'type': dataFromUser.my_type,
+        'ratingFrom': dataFromUser.ratingFrom,
+        'yearFrom': dataFromUser.yearFrom,
+        'yearTo': dataFromUser.yearTo,
+    }
+    keys_of_dict = ['items', 'kinopoiskId', 'ratingKinopoisk']
+elif choice == 2:
+    url_film_info = 'https://kinopoiskapiunofficial.tech/api/v2.1/films/' \
+                    'search-by-keyword'
+    dataFromUser: RequestData = userCommunicator.dataRequestFromUserByKeywords()
+    params = {
+        'keyword': dataFromUser.keywords
+    }
+    keys_of_dict = ['films', 'filmId', 'rating']
+
 apiRequester: ApiRequester = ApiRequester()
 json_response = apiRequester.callApi(url_film_info, params)
 
 
 print("Вот, что ты можешь посмотреть:")
-for slovar in json_response["items"]:
-    print(slovar["kinopoiskId"], slovar["nameRu"])
+
+for slovar in json_response[keys_of_dict[0]]:
+    print(slovar[keys_of_dict[1]], slovar["nameRu"])
+
 
 print("\n")
-id = input("О какой из этих работ ты хотел бы узнать больше?\nВведи id фильма (цифры перед названием):\n")
-url_film_awards = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/'+id+'/awards'
+id = input("О какой из этих работ ты хотел бы узнать больше?\n"
+           "Введи id фильма (цифры перед названием):\n")
 
+wanted_film = {}
+found = False
+for film in json_response[keys_of_dict[0]]:
+    if film[keys_of_dict[1]] == int(id):
+        wanted_film = film
+        found = True
 
+while not found:
+    id = input("В найденом списке нет введенного id.\n"
+           "Пожалуйста, введи id фильма (цифры перед названием) из списка:\n")
+    for film in json_response[keys_of_dict[0]]:
+        if film[keys_of_dict[1]] == int(id):
+            wanted_film = film
+            found = True
+
+url_film_awards = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/'\
+                  + id + '/awards'
 params = {
     "id": id
 }
 
 json_awards_response = apiRequester.callApi(url_film_awards, params)
 
+message = ""
+if wanted_film.get('nameRu'):
+    message += f"Название фильма: {wanted_film['nameRu']}\n"
+if wanted_film.get(keys_of_dict[2]):
+    message += f"Рейтинг фильма: {wanted_film[keys_of_dict[2]]}\n"
+if wanted_film.get('year'):
+    message += f"Год выхода: {wanted_film['year']}\n"
+if wanted_film.get('countries'):
+    message += f"Страна: {wanted_film['countries'][0]['country']}\n"
+if wanted_film.get('genres'):
+    message += f"Жанр: {wanted_film['genres'][0]['genre']}\n"
+if wanted_film.get('description'):
+    message += f"Описание:\n{wanted_film['description']}"
+
+print(message)
 print("Это произведение получило следующие награды:")
 for slovar in json_awards_response["items"]:
     print(f'{slovar["name"]}. {slovar["nominationName"]}.')
+if len(json_awards_response["items"]) == 0:
+    print("Нет наград")
 
 
 
